022825c7b7dde0b43e024fea37cc348b425975ca
Pontus Croneld <pontus.croneld@gmail.com>
2024-10-03 09:44:29 +0200

    Initial Commit

---
+++ UmainCodeTest/UmainCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmainCodeTestApp.swift
+//  UmainCodeTest
+//
+//  Created by Pontus Croneld on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct UmainCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
